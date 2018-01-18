f82b24b8bb97128b66aa911af52957060954797a
konstantin0s <constantintofan85@gmail.com>
2018-01-18 23:01:17 +0100

    Add style and components. Finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import List from './List';
 
 
 class App extends Component {
@@ -9,12 +11,28 @@ constructor(props){
   this.state = {
     people: [],
   }
+  this.getPeople.bind(this);
 }
 
+getPeople(){
+  return axios.get("https://swapi.co/api/people")
+  .then((response) => {
+console.log(response.data.results);
+this.setState( {people: response.data.results} )
+  })
+}
+
+componentDidMount(){
+  this.getPeople()
+}
+
+
   render() {
+    const {people} = this.state;
+
     return (
       <div className="App">
-
+  <List people={people} />
       </div>
     );
   }
